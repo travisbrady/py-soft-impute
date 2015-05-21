b6e28effbdc4bce5f96caf23e9db308d5f9ed5cd
Travis Brady <travis.brady@gmail.com>
2015-05-20 19:22:20 -0500

    checkpoint. working code

--- soft_impute.py
+++ soft_impute.py
@@ -14,19 +14,20 @@ class SoftImpute:
         self.u = None
         self.d = None
         self.v = None
+
     def fit(self, X, J=2, thresh=1e-05, _lambda=0, maxit=100):
         n,m = X.shape
         xnas = np.isnan(X)
         nz = m*n - xnas.sum()
-        xfill = X
+        xfill = X.copy()
         V = np.zeros((m, J))
         U = np.random.normal(0.0, 1.0, (n, J))
         U, _, _ = np.linalg.svd(U, full_matrices=False)
-        Dsq = np.ones((1, J))
+        #Dsq = np.ones((1, J))
+        Dsq = np.ones((J, 1))
         xfill[xnas] = 0.0
         ratio = 1.0
         while ratio > thresh and maxit > 0:
-            print 'maxit = {}'.format(maxit)
             maxit -= 1
             U_old = U
             V_old = V
@@ -34,7 +35,7 @@ class SoftImpute:
             B = U.T.dot(xfill)
 
             if _lambda > 0:
-                tmp = (Dsq / (Dsq + _lambda)).T
+                tmp = (Dsq / (Dsq + _lambda))
                 B = B * tmp
 
             Bsvd = np.linalg.svd(B.T, full_matrices=False)
@@ -56,37 +57,52 @@ class SoftImpute:
 
             xhat = U.dot(tmp)
             xfill[xnas] = xhat[xnas]
-            ratio =frob(U_old, Dsq_old, V_old, U, Dsq, V)
+            ratio = frob(U_old, Dsq_old, V_old, U, Dsq, V)
+            print 'ratio = {:.5f} maxit = {}'.format(ratio, maxit)
+
         self.u = U[:,:J]
         self.d = Dsq[:J]
         self.v = V[:,:J]
         self.lambda_ = _lambda
         return self
 
-    def suv(self, vd):
-        r = []
-        print vd.shape
-        print self.u.shape
-        return self.u.T.dot(vd)
+    def suv(self, X, vd):
+        X_imp = X.copy()
+        res = self.u.dot(vd.T)
+        np.copyto(X_imp, res, where=np.isnan(X_imp))
+        '''
+        for i in range(X.shape[0]):
+            for j in range(X.shape[1]):
+                if np.isnan(X[i, j]):
+                    #yex = self.u[i,:].dot(vd.T[j,:])
+                    #yex = self.u[i,:] * vd[:,j]
+                    yex = self.u.dot(vd.T)[i, j]
+                    X_imp[i, j] = yex
+        '''
+        return X_imp
 
     def predict(self, X):
-        vd = np.outer(np.ones(self.v.shape[0]), self.d)
-        out = self.suv(vd)
+        vd = self.v * np.outer(np.ones(self.v.shape[0]), self.d)
+        out = self.suv(X, vd)
         return out
 
 def main():
-    X = np.random.random((10,3)) + np.arange(10).reshape(10,1)
-    #outs = 'matrix(' + str(b.tolist()).replace('[', 'c(').replace(']', ')')
-    #outs += ',nrow=%d, ncol=%d)' % (b.shape[0], b.shape[1])
+    X = np.random.random((10,3)) + (np.arange(10).reshape(10,1) ** 2)
+    #X = np.arange(100).reshape(25, 4).astype(np.float32)
 
     clf = SoftImpute()
-    fit = clf.fit(X, _lambda=0.0)
-    print
+    fit = clf.fit(X, J=2, _lambda=0.0)
     print fit.u
     print fit.d
     print fit.v
+    X_test = X.copy()
+    X_test[3,1] = np.nan
+    X_imp = clf.predict(X_test)
+    print 'XXX'
+    print X_test[:5]
     print
-    print clf.predict(X)
+    print 'X_imp'
+    print X_imp[:5]
 
 if __name__ == '__main__':
     main()
