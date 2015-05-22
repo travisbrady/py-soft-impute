99d46b8cda717dc36cf2c47725aa55b6f739e4ed
Travis Brady <travis.brady@gmail.com>
2015-05-22 18:30:10 -0500

    sklearn-ify interface. Add experimental NA initialization to mean

--- soft_impute.py
+++ soft_impute.py
@@ -10,32 +10,40 @@ def frob(Uold, Dsqold, Vold, U, Dsq, V):
     return num / max(denom, 1e-9)
 
 class SoftImpute:
-    def __init__(self):
+    def __init__(self, J=2, thresh=1e-05, lambda_=0, maxit=100, random_state=None, verbose=False):
+        self.J = J
+        self.thresh = thresh
+        self.lambda_ = lambda_
+        self.maxit = maxit
+        self.rs = np.random.RandomState(random_state)
+        self.verbose = verbose
         self.u = None
         self.d = None
         self.v = None
 
-    def fit(self, X, J=2, thresh=1e-05, _lambda=0, maxit=100):
+    def fit(self, X):
         n,m = X.shape
         xnas = np.isnan(X)
         nz = m*n - xnas.sum()
         xfill = X.copy()
-        V = np.zeros((m, J))
-        U = np.random.normal(0.0, 1.0, (n, J))
+        V = np.zeros((m, self.J))
+        U = self.rs.normal(0.0, 1.0, (n, self.J))
         U, _, _ = np.linalg.svd(U, full_matrices=False)
-        #Dsq = np.ones((1, J))
-        Dsq = np.ones((J, 1))
-        xfill[xnas] = 0.0
+        Dsq = np.ones((self.J, 1))
+        #xfill[xnas] = 0.0
+        col_means = np.nanmean(xfill, axis=0)
+        np.copyto(xfill, col_means, where=np.isnan(xfill))
         ratio = 1.0
-        while ratio > thresh and maxit > 0:
-            maxit -= 1
+        iters = 0
+        while ratio > self.thresh and iters < self.maxit:
+            iters += 1
             U_old = U
             V_old = V
             Dsq_old = Dsq
             B = U.T.dot(xfill)
 
-            if _lambda > 0:
-                tmp = (Dsq / (Dsq + _lambda))
+            if self.lambda_ > 0:
+                tmp = (Dsq / (Dsq + self.lambda_))
                 B = B * tmp
 
             Bsvd = np.linalg.svd(B.T, full_matrices=False)
@@ -58,51 +66,34 @@ class SoftImpute:
             xhat = U.dot(tmp)
             xfill[xnas] = xhat[xnas]
             ratio = frob(U_old, Dsq_old, V_old, U, Dsq, V)
-            print 'ratio = {:.5f} maxit = {}'.format(ratio, maxit)
+            if self.verbose:
+                print 'ratio = {:.6f} iters = {:4d}'.format(ratio, iters)
 
-        self.u = U[:,:J]
-        self.d = Dsq[:J]
-        self.v = V[:,:J]
-        self.lambda_ = _lambda
+        self.u = U[:,:self.J]
+        self.d = Dsq[:self.J]
+        self.v = V[:,:self.J]
         return self
 
-    def suv(self, X, vd):
-        X_imp = X.copy()
+    def suv(self, vd):
         res = self.u.dot(vd.T)
-        np.copyto(X_imp, res, where=np.isnan(X_imp))
-        '''
-        for i in range(X.shape[0]):
-            for j in range(X.shape[1]):
-                if np.isnan(X[i, j]):
-                    #yex = self.u[i,:].dot(vd.T[j,:])
-                    #yex = self.u[i,:] * vd[:,j]
-                    yex = self.u.dot(vd.T)[i, j]
-                    X_imp[i, j] = yex
-        '''
-        return X_imp
+        return res
 
-    def predict(self, X):
+    def predict(self, X, copyto=False):
         vd = self.v * np.outer(np.ones(self.v.shape[0]), self.d)
-        out = self.suv(X, vd)
-        return out
+        X_imp = self.suv(vd)
+        if copyto:
+            np.copyto(X, X_imp, where=np.isnan(X))
+        else:
+            return X_imp
 
 def main():
     X = np.random.random((10,3)) + (np.arange(10).reshape(10,1) ** 2)
-    #X = np.arange(100).reshape(25, 4).astype(np.float32)
 
-    clf = SoftImpute()
-    fit = clf.fit(X, J=2, _lambda=0.0)
-    print fit.u
-    print fit.d
-    print fit.v
+    clf = SoftImpute(J=2, lambda_=0.0)
+    fit = clf.fit(X)
     X_test = X.copy()
     X_test[3,1] = np.nan
     X_imp = clf.predict(X_test)
-    print 'XXX'
-    print X_test[:5]
-    print
-    print 'X_imp'
-    print X_imp[:5]
 
 if __name__ == '__main__':
     main()
