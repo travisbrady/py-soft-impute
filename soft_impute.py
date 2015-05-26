25561712fe1071d605291e414e15deb3717e2994
Travis Brady <travis.brady@gmail.com>
2015-05-26 18:51:24 -0500

    add license header

--- soft_impute.py
+++ soft_impute.py
@@ -1,3 +1,19 @@
+'''
+   Copyright 2015 Travis Brady
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+from __future__ import print_function
 import numpy as np
 
 
@@ -67,7 +83,7 @@ class SoftImpute:
             xfill[xnas] = xhat[xnas]
             ratio = frob(U_old, Dsq_old, V_old, U, Dsq, V)
             if self.verbose:
-                print 'ratio = {:.6f} iters = {:4d}'.format(ratio, iters)
+                print('iter: %4d ratio = %.5f' % (iters, ratio))
 
         self.u = U[:,:self.J]
         self.d = Dsq[:self.J]
